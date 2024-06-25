ee6bb1e5bb742279c6b3520fe9b2542bb7712f36
eujin31 <eujin31@gmail.com>
2024-06-25 20:57:46 +0900

    feat:10866

---
+++ Baekjoon/10866.py
@@ -0,0 +1,54 @@
+'''
+문제 링크:https://www.acmicpc.net/problem/10866
+카테고리:덱
+문제해설:덱(Deque)를 구현한 다음, 입력으로 주어지는 명령을 처리
+'''
+from collections import deque
+import sys
+
+n=int(input())
+input=sys.stdin.readline
+
+d=deque([])
+
+for i in range(n):
+  commend=input().split()
+
+  if commend=='push_front':
+    d.appendleft()
+
+  if commend=='push_back':
+    d.append()
+
+  if commend=='pop_front':
+    if d:
+      print(d.popleft())
+    else:
+      print(-1)
+
+  if commend=='pop_back':
+    if d:
+      print(d.pop())
+    else:
+      print(-1)
+
+  if commend=='size':
+    print(len(d))
+
+  if commend=='empty':
+    if d:
+      print(0)
+    else:
+      print(1)
+
+  if commend=='front':
+    if d:
+      print(d[0])
+    else:
+      print(-1)
+
+  if commend=='back':
+    if d:
+      print(d[n-1])
+    else:
+      print(-1)
