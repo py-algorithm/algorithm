7437e4269daf979b6c4139e22ef52ac0a7b1a04b
eujin31 <eujin31@gmail.com>
2024-06-26 23:30:23 +0900

    feat:2588해결완료

---
+++ Baekjoon/2588.py
@@ -0,0 +1,17 @@
+'''
+문제 링크: https://www.acmicpc.net/problem/2753
+카테고리: 수학
+문제 해설: (세 자리 수) × (세 자리 수)
+'''
+a=int(input())
+b=int(input())
+
+arr=[a*(b%10),
+     a*(b%100-b%10),
+     a*(b-b%100),
+     a*(b%10)+a*(b%100-b%10)+a*(b-b%100)]
+
+print(arr[0])
+print(arr[1]//10)
+print(arr[2]//100)
+print(arr[3])
