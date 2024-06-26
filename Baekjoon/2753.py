b0f5728264df07d1f80586ed517a1ceacc08018a
eujin31 <eujin31@gmail.com>
2024-06-26 23:05:12 +0900

    feat:2753해결완료

---
+++ Baekjoon/2753.py
@@ -0,0 +1,14 @@
+'''
+문제 링크: https://www.acmicpc.net/problem/2753
+카테고리: 수학
+문제 해설: 윤년을 판별합니다.
+'''
+
+n=int(input())
+
+if n%4==0 and n%100!=0:
+  print(1)
+elif n%400==0:
+  print(1)
+else:
+  print(0)
