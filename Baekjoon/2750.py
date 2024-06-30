3da19c2dab45532b631a4e42099be2cfc7526797
eujin31 <eujin31@gmail.com>
2024-06-30 20:12:04 +0900

    fix: int로 캐스팅

--- Baekjoon/2750.py
+++ Baekjoon/2750.py
@@ -4,9 +4,9 @@
 문제 해설: 오름차순으로 정렬합니다.
 '''
 
-'입출력핵심'
+# 입출력핵심
 n=int(input())
-m=[input() for _ in range(n)]#n개한줄 씩 입력받기
+m=[int(input()) for _ in range(n)]#n개한줄 씩 입력받기
 l=sorted(m)
 
 print(*l,sep='\n')#l의 요소들 한줄씩 출력
