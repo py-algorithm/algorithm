bdb995c1f6d5dc5c03671e21d0614db8652fbb81
cakejin <122671200+cakejin@users.noreply.github.com>
2024-07-01 11:06:58 +0900

    [백준] 2750 (#13)
    * feat: 2750 입출력
    
    * fix: int로 캐스팅

---
+++ Baekjoon/2750.py
@@ -0,0 +1,12 @@
+'''
+문제 링크: https://www.acmicpc.net/problem/2750
+카테고리: 정렬
+문제 해설: 오름차순으로 정렬합니다.
+'''
+
+# 입출력핵심
+n=int(input())
+m=[int(input()) for _ in range(n)]#n개한줄 씩 입력받기
+l=sorted(m)
+
+print(*l,sep='\n')#l의 요소들 한줄씩 출력
