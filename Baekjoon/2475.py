89ccb18d406f6cafa705520b96bfc806a3c4a9f9
eujin31 <eujin31@gmail.com>
2024-07-13 16:05:31 +0900

    feat: lambda사용해보기

--- Baekjoon/2475.py
+++ Baekjoon/2475.py
@@ -3,10 +3,17 @@
 카테고리:수학, 구현, 사칙연산
 문제해설:주어진 조건대로 연산된 검증수 출력하기
 '''
-a=map(int,input().split())
+'''a=map(int,input().split())
 result=0
 
 for i in a:
   result+=i**2
 
 print(result%10)
+'''
+
+from functools import reduce
+
+ret=reduce(lambda acc, cur : acc + cur**2, map(int,input().split()),0)%10
+
+print(ret)
