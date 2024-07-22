374a6dd6695fcbda6c841d5b968d349d70ebc459
cakejin <122671200+cakejin@users.noreply.github.com>
2024-07-22 21:04:10 +0900

    [백준 1546] <수학, 사칙연산>: 조작된 성적 평균 구하기 (#29)

---
+++ Baekjoon/1546.py
@@ -0,0 +1,15 @@
+'''
+문제 링크:https://www.acmicpc.net/problem/1546
+카테고리:수학, 사칙연산
+문제해설: 조작된 성적 평균 구하기
+'''
+a=int(input())
+b=list(map(int,input().split()))
+max_b=max(b)
+
+result=0
+
+for i in b:
+  result+=i/max_b*100
+
+print(result/len(b))
