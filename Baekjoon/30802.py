13a1f1874cae1c290d65ddbb70c02b1b260b71ff
eujin31 <eujin31@gmail.com>
2024-07-28 23:38:15 +0900

    feat: 30802 해결완료

--- Baekjoon/30802.py
+++ Baekjoon/30802.py
@@ -5,10 +5,8 @@
 '''
 #t의 최소묶음 및 p(남으면 안됨)최대 묶음과 한자루씩 몇개 추가하는지
 n=int(input())
-num_size=[map(int,input().split())]
+num_size=list(map(int,input().split())) #list()말고 []쓰면 오류
 t,p=map(int,input().split())
-
-num_size=[int() for  _ in num_size]#리스트 요소들 정수로 변경
 
 result_t=0 #티셔츠 묶음 수 초기화
 
