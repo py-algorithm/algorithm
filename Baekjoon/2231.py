3c141c566e40ad52db78172c12064b49ba200eec
eujin31 <eujin31@gmail.com>
2024-08-06 17:38:08 +0900

    feat: 문제해설 수정

--- Baekjoon/2231.py
+++ Baekjoon/2231.py
@@ -2,14 +2,15 @@
 문제 링크:https://www.acmicpc.net/problem/2231
 카테고리:브루트포스
 문제해설:분해합의 생성자 구하기
+
+n=m+m//100+(m-m//100)//10+(m-m//100-..)->resursive?
+
+찾은m을 재귀적으로 n에 넣기
+
+m을 1부터n까지 완전탐색으로 찾기
 '''
 n=int(input())
 
-#n=m+m//100+(m-m//100)//10+(m-m//100-..)->resursive?
-
-#찾은m을 재귀적으로 n에 넣기
-
-#m을 1부터n까지 완전탐색으로 찾기
 for m in range(1,n+1):
   a=sum(map(int,str(m)))
   m_sum=m+a 
