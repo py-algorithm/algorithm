f742857c94a1fd565b102cb692e87a0507cc49f4
eujin31 <eujin31@gmail.com>
2024-08-21 17:04:39 +0900

    fix: 메모리 초과를 랜덤 함수로 해결

--- Baekjoon/2751.py
+++ Baekjoon/2751.py
@@ -2,9 +2,15 @@
 문제 링크:https://www.acmicpc.net/problem/2751
 카테고리: 정렬
 문제해설: 수 정렬하기2
+
+선택정렬n^2
+퀵정렬nlogn->메모리초과!
+계수정렬n->동인한 값 반복 등장시 효율적
+
 '''
 
 import sys
+import random
 
 input=sys.stdin.readline
 
@@ -12,6 +18,20 @@ n=int(input())
 
 m=[int(input()) for _ in range(n)]
 
-arr=sorted(m)
+def quick_sort(array):
+  if len(array) <= 1:
+    return array
+  
+  pivot = random.choice(array)
 
-print(*arr,sep='\n')
+  left_side = [x for x in array if x <= pivot]
+  right_side = [x for x in array if x > pivot]
+
+  return quick_sort(left_side) + quick_sort(right_side)
+  
+print(*quick_sort(m), sep='\n')
+
+
+
+
+
