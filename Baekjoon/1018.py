7e31f8e3fe9acaf20320962a6a752ed62b5a15e0
eujin31 <eujin31@gmail.com>
2024-09-02 17:41:59 +0900

    feat: 1018 해결중

--- Baekjoon/1018.py
+++ Baekjoon/1018.py
@@ -2,76 +2,87 @@
 문제 링크:https://www.acmicpc.net/problem/1018
 카테고리: 브루트포스 알고리즘
 문제해설: 체스판 다시 칠하기
+
+dfs탐색이용-큐자료구조, 최단경로
+
+<입력된 NxM크기의 보드를 잘라서 
+8x8크기의 체스판 만들기위해 다시 
+색칠해야하는 정사각형 최소 개수 구하기>
+
+1. board 2차원 행렬로 입력받기
+2. 브루트포스로 8x8나올 수 있는 모든 경우 확인
+3. 함수에 board[i][j]시작으로 하는 8x8에서 색칠해야하는 횟수 구하기
+  시작이 w,b일때 각각 나눠서
 '''
-#dfs탐색이용-재귀적
-'''
-<w의 상하좌우는 b, b의 상하좌우는 w이어야함>
-<(1,1)부터(n,n)까지 모든 경우에서의 다시 칠하는 수 비교하기>
-'''
-n,m=map(int,input().split())
-board=[]
-result=[]
+import sys
+
+input = sys.stdin.readline
+
+#NxM
+n, m = map(int, input().split())
+
+
+
+#입력받은 보드 상태->2차원 배열
+board = []
 
 for _ in range(n):
-  board.append(list(map(input())))
+  board.append(list(map(str,input())))
 
-visited=board.copy()
+#각 경우의 값 리스트에 넣어서 최소값만 출력하기
+result = sys.maxsize
 
-#시작점(i,j)에서 색칠변수선언
+#시작점(i, j)일때 8X8까지만 범위정해서 다시 칠해야하는 칸 개수 세기
+def chess(row,col):
+  cnt=0
+
+  is_white=board[row][col]=='W'
+  
+  for i in range(row, row+8):
+    for j in range(col, col+8):
+        
+      #첫 칸 W
+      if is_white:
+          #W는 홀수번째 행 + 홀수번째 열 or 짝수번째 행 + 짝수번째 열
+          if i % 2 == 1 and j % 2 == 1:
+            if board[i][j] == 'B':
+              cnt += 1
+          if i % 2 ==0 and j % 2 == 0:
+            if board[i][j] == 'B':
+              cnt += 1
+          if i % 2 ==1 and j % 2 == 0:
+            if board[i][j] == 'W':
+              cnt += 1
+          if i % 2 == 0 and j % 2 == 1:
+            if board[i][j] == 'W':
+              cnt += 1
+            
+
+      #첫 칸 B
+      else:
+          #B는 홀수번째 행 + 홀수번째 열 or 짝수번째 행 + 짝수번째 열
+          if i % 2 == 1 and j % 2 == 1:
+            if board[i][j] == 'W':
+              cnt += 1
+          if i % 2 ==0 and j % 2 == 0:
+            if board[i][j] == 'W':
+              cnt += 1
+          if i % 2 ==1 and j % 2 == 0:
+            if board[i][j] == 'B':
+              cnt += 1
+          if i % 2 == 0 and j % 2 == 1:
+            if board[i][j] == 'B':
+              cnt += 1
+
+  return cnt
+
+
+
 for i in range(n-7):
   for j in range(m-7):
-    paint_1=0 #시작이 w일때
-    paint_2=0 #시작이 b일때
+    result=min(result, chess(i, j))
+
+    print(i, j, result)
+    
 
 
-def dfs(x,y):
-  global paint_1
-  global paint_2
-
-  if x<=-1 or x>m or y<=-1 or y>n:
-    return False
-  
-  if board[x][y]=='w' and visited[x][y]=='w':
-    visited[x][y+1]=='b'
-
-    if visited[x][y+1]=='w':
-      paint_1+=1
-
-    visited[x][y-1]=='b'
-    if visited[x][y-1]=='w':
-      paint_1+=1
-
-    visited[x-1][y]=='b'
-    if visited[x-1][y]=='w':
-      paint_1+=1
-      
-    visited[x+1][y]=='b'
-    if visited[x+1][y]=='w':
-      paint_1+=1
-
-    return True
-  
-  if board[x][y]=='b' and visited[x][y]=='b':
-    visited[x][y+1]=='w'
-
-    if visited[x][y+1]=='b':
-      paint_2+=1
-
-    visited[x][y-1]=='w'
-    if visited[x][y-1]=='b':
-      paint_2+=1
-
-    visited[x-1][y]=='w'
-    if visited[x-1][y]=='b':
-      paint_2+=1
-      
-    visited[x+1][y]=='w'
-    if visited[x+1][y]=='b':
-      paint_2+=1
-      
-    return True
-  
-  result.append(min(paint_1,paint_2))
-
-print(min(result))
-  
