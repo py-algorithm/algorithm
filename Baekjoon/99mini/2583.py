1ae0c82d7c2886f3541ffef256a3c030d8395180
99mini <itman390@gmail.com>
2024-10-22 19:04:35 +0900

    feat: 영역 구하기 2583
    dfs

---
+++ Baekjoon/99mini/2583.py
@@ -0,0 +1,62 @@
+'''
+작성자: 99mini
+문제 링크:https://www.acmicpc.net/problem/2583
+카테고리: 그래프 탐색
+문제해설:
+TODO: `global` 변수 사용 안하기
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**6)
+
+m, n, k = map(int, input().split())
+
+grid = [[False] * n for _ in range(m)]
+
+for _ in range(k):
+    bottom_left_x, bottom_left_y, top_right_x, top_right_y = map(int, input().split())
+
+    for x in range(bottom_left_x, top_right_x):
+        for y in range(bottom_left_y, top_right_y):
+            grid[y][x] = True
+
+count = 0
+global area
+area = 0
+
+area_list = []
+
+visited = [[False] * n for _ in range(m)]
+
+delta_x_unit = [0, 0, 1, -1]
+delta_y_unit = [1, -1, 0, 0]
+
+def dfs(x, y):
+    visited[y][x] = True
+    global area
+    area += 1
+
+    for dx, dy in zip(delta_x_unit, delta_y_unit):
+        cx = x + dx
+        cy = y + dy
+
+        if ( cx >= 0 and cx < n ) and ( cy >= 0 and cy < m ) and not visited[cy][cx] and not grid[cy][cx]:
+            dfs(cx, cy)
+
+
+for x in range(n):
+    for y in range(m):
+        if not visited[y][x] and not grid[y][x]:
+            count += 1
+            
+            dfs(x, y)
+
+            area_list.append(area)
+            area = 0
+
+print(count)
+print(*sorted(area_list))
+
