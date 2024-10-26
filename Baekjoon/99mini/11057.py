67835723c09339ff552b8493a7641778c2d166a6
99mini <43674669+99mini@users.noreply.github.com>
2024-10-26 16:31:40 +0900

    [백준] 실버 문제 부수기 (#64)
    * feat: 1018
    
    * feat: 1094 비트 연산
    
    * feat: 3190 입력
    
    * fix: delete gold problem
    
    * feat: N과 M (9)
    
    백트레킹 문제
    
    * feat: 회의실 배정 1931
    
    그리디 알고리즘
    
    * feat: 영역 구하기 2583
    
    dfs
    
    * fix: replace global to dict
    
    * feat: 오르막 수 11057
    
    dp
    
    * fix(1094): `format` to `bin`
    
    * feat(1389): 플로이드 와샬
    
    * 1926: DFS

---
+++ Baekjoon/99mini/11057.py
@@ -0,0 +1,32 @@
+'''
+작성자: 99mmini
+문제 링크: https://www.acmicpc.net/problem/11057
+카테고리: 다이나믹 프로그래밍
+문제 해설:
+
+dp[N][10]
+
+n   0   1   2   3   4   5   6   7   8   9
+1   1   1   1   1   1   1   1   1   1   1
+2   1   2   3   ...                     10
+3   1   3   6   ...
+
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [[1 if i == 0 or j == 0 else 0 for j in range(10)] for i in range(n)]
+
+for i in range(1, n):
+    suffix_sum = [0] * 10
+    suffix_sum[0] = 1
+
+    for j in range(1, 10):
+        suffix_sum[j] = suffix_sum[j-1] + dp[i - 1][j]
+        dp[i][j] = suffix_sum[j]
+
+print(sum(dp[n-1]) % 10007)
---
+++ Baekjoon/99mini/1926.py
@@ -0,0 +1,45 @@
+'''
+작성자: 99mmini
+문제 링크: https://www.acmicpc.net/problem/1926
+카테고리: 그래프
+문제 해설
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+sys.setrecursionlimit(10**6)
+
+n, m = map(int, input().split())
+
+metrix = [list(map(int, input().split())) for _ in range(n)]
+
+visited = [[False] * m for _ in range(n)]
+
+pictures = dict()
+count = 0
+
+drow = [0, 0, 1, -1]
+dcol = [1, -1, 0, 0]
+
+def dfs(row, col):
+    visited[row][col] = True
+    pictures[count] += 1
+
+    for dr, dc in zip(drow, dcol):
+        nr = row + dr
+        nc = col + dc
+
+        if 0 <= nr < n and 0 <= nc < m and metrix[nr][nc] and not visited[nr][nc]:
+            dfs(nr, nc)
+
+for row in range(n):
+    for col in range(m):
+        if metrix[row][col] and not visited[row][col]:
+            count += 1
+            pictures[count] = 0
+            dfs(row, col)
+
+print(count)
+print(max(list(pictures[p] for p in pictures)) if count else 0)
