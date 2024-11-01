a5c681e13ab976876c2637334d855073eb7300ad
99mini <itman390@gmail.com>
2024-11-01 17:36:06 +0900

    1074: 재귀 대신 ad hoc

---
+++ Baekjoon/99mini/1074.py
@@ -0,0 +1,40 @@
+'''
+작성자: 99mini
+문제 링크: https://www.acmicpc.net/problem/1074
+카테고리: 재귀, 분할 정복, ad-hoc
+문제 해설:
+'''
+
+import sys
+
+input = sys.stdin.readline
+
+N, r, c = map(int, input().split())
+
+ans = 0
+
+while N != 0:
+
+	N -= 1
+
+	# 제2사분면
+	if r < 2 ** N and c < 2 ** N:
+		ans += ( 2 ** N ) * ( 2 ** N ) * 0
+
+	# 제1사분면
+	elif r < 2 ** N and c >= 2 ** N: 
+		ans += ( 2 ** N ) * ( 2 ** N ) * 1
+		c -= ( 2 ** N )
+        
+	# 제3사분면    
+	elif r >= 2 ** N and c < 2 ** N: 
+		ans += ( 2 ** N ) * ( 2 ** N ) * 2
+		r -= ( 2 ** N )
+        
+	# 제4사분면    
+	else:
+		ans += ( 2 ** N ) * ( 2 ** N ) * 3
+		r -= ( 2 ** N )
+		c -= ( 2 ** N )
+    
+print(ans)
