3a9ceb3afa76e861348da2ca2584ffa937770378
99mini <itman390@gmail.com>
2024-11-04 15:40:28 +0900

    refactor: optimize BFS implementation for wall-breaking in problem 14442

--- Baekjoon/99mini/14442.py
+++ Baekjoon/99mini/14442.py
@@ -17,51 +17,51 @@ n, m, k = map(int, input().split())
 
 graph = [list(map(int, input().strip())) for _ in range(n)]
 
-# [dict({벽을 부순 횟수: 길이})]
-visited: list[dict[int, int]] = [[dict() for _ in range(m)] for _ in range(n)]
-visited[0][0][0] = 1
+visited = [[[0] * (k + 1) for _ in range(m)] for _ in range(n)]
+visited[0][0][k] = 1
 
-# (row, col, break_cnt)
-que = deque([(0, 0, 0)])
+def bfs():
 
-while que:
-    r, c, break_cnt = que.popleft()
-
-    if r == n - 1 and c == m - 1:
-        break
-    
-    distance = visited[r][c][break_cnt]
-    
-    for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-        nr = r + dr
-        nc = c + dc
-
-        # graph의 범위를 넘은 경우
-        if nr < 0 or nr >= n or nc < 0 or nc >= m:
-            continue
-
-        # 벽일 경우
-        if graph[nr][nc]:
-            # 벽을 부순 횟수가 최대값인 경우
-            if break_cnt == k:
-                continue
-
-            # 방문했으면
-            if break_cnt + 1 in visited[nr][nc]:
-                continue
-
-            visited[nr][nc][break_cnt+1] = distance + 1
-            que.append((nr, nc, break_cnt + 1))
+    # (row, col, break_cnt)
+    que = deque([(0, 0, k)])
+    while que:
+        r, c, break_cnt = que.popleft()
         
-        # 벽이 아닌 경우
-        else:
-            # 방문했으면
-            if break_cnt in visited[nr][nc]:
+        distance = visited[r][c][break_cnt]
+
+        if r == n - 1 and c == m - 1:
+            return distance
+        
+        for dr, dc in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+            nr = r + dr
+            nc = c + dc
+
+            # graph의 범위를 넘은 경우
+            if nr < 0 or nr >= n or nc < 0 or nc >= m:
                 continue
-            visited[nr][nc][break_cnt] = distance + 1
-            que.append((nr, nc, break_cnt))
 
+            # 벽일 경우
+            if graph[nr][nc]:
+                # 벽을 부술 수 있는 잔여 횟수가 0안 경우
+                if break_cnt == 0:
+                    continue
+
+                # 방문했으면
+                if visited[nr][nc][break_cnt - 1]:
+                    continue
+
+                visited[nr][nc][break_cnt - 1] = distance + 1
+                que.append((nr, nc, break_cnt - 1))
+            
+            # 벽이 아닌 경우
+            else:
+                # 방문했으면
+                if visited[nr][nc][break_cnt]:
+                    continue
+                visited[nr][nc][break_cnt] = distance + 1
+                que.append((nr, nc, break_cnt))
+    
+    return -1
+
+print(bfs())
 print(*visited, sep='\n')
-
-ret = min(visited[n - 1][m - 1][v] for v in visited[n - 1][m - 1]) if visited[n - 1][m - 1] else -1
-print(ret)
