3ea900f967d2b003a10c378857e0f62741e78f9a
99mini <itman390@gmail.com>
2024-11-04 15:11:57 +0900

    refactor: update BFS logic for wall-breaking in problem 14442

--- Baekjoon/99mini/14442.py
+++ Baekjoon/99mini/14442.py
@@ -3,7 +3,7 @@
 문제 링크:https://www.acmicpc.net/problem/14442
 카테고리: BFS
 문제해설: 
-벽 뚫고 이동하기 2
+벽 부수고 이동하기 2
 '''
 
 import sys
@@ -21,39 +21,45 @@ graph = [list(map(int, input().strip())) for _ in range(n)]
 visited: list[dict[int, int]] = [[dict() for _ in range(m)] for _ in range(n)]
 visited[0][0][0] = 1
 
-que = deque([(0, 0)])
+# (row, col, break_cnt)
+que = deque([(0, 0, 0)])
 
 while que:
-    r, c = que.popleft()
+    r, c, break_cnt = que.popleft()
 
-    curr_visited = visited[r][c]
-
+    if r == n - 1 and c == m - 1:
+        break
+    
+    distance = visited[r][c][break_cnt]
+    
     for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         nr = r + dr
         nc = c + dc
 
+        # graph의 범위를 넘은 경우
         if nr < 0 or nr >= n or nc < 0 or nc >= m:
             continue
+
+        # 벽일 경우
+        if graph[nr][nc]:
+            # 벽을 부순 횟수가 최대값인 경우
+            if break_cnt == k:
+                continue
+
+            # 방문했으면
+            if break_cnt + 1 in visited[nr][nc]:
+                continue
+
+            visited[nr][nc][break_cnt+1] = distance + 1
+            que.append((nr, nc, break_cnt + 1))
         
-        for v in curr_visited:
-            distance = curr_visited[v]
-            # 벽일 경우
-            if graph[nr][nc]:
-                # 벽을 뚫은 횟수가 최대값인 경우
-                if v == k:
-                    continue
-                if v+1 in visited[nr][nc]:
-                    continue
-                visited[nr][nc][v+1] = distance + 1
-                que.append((nr, nc))
-            
-            # 벽이 아닌 경우
-            else:
-                if v in visited[nr][nc]:
-                    continue
-                else:
-                    visited[nr][nc][v] = distance + 1
-                    que.append((nr, nc))
+        # 벽이 아닌 경우
+        else:
+            # 방문했으면
+            if break_cnt in visited[nr][nc]:
+                continue
+            visited[nr][nc][break_cnt] = distance + 1
+            que.append((nr, nc, break_cnt))
 
 print(*visited, sep='\n')
 
