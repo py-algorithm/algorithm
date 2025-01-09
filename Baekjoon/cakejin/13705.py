67aa9d63383903235be3a233cb3244200cf4a969
eujin31 <eujin31@gmail.com>
2025-01-09 15:50:48 +0900

    fix: 주석 수정

--- Baekjoon/cakejin/13705.py
+++ Baekjoon/cakejin/13705.py
@@ -9,16 +9,14 @@ A, B, C가 주어졌을 때, Ax+Bsin(x)=C를 만족하는 x를 찾는 프로그�
 출력: 소수점 여섯째 자리까지 출력(반올림)
 
 f(x)=Ax+Bsin(x)-C=0의 수치해 찾기
-f'(x)=A+Bcos(x)
 
 해가 존재하는 구간 모름: a1,b1사이에 존재한다 가정
                         (f(a1)f(b1) < 0 인 a1,b1찾기)
-                        f(x): 증가함수 -> Ax+Bsin(x) 의 해는 0근방
-                            f(x)의 해는 C근방
+                        f(x): 증가함수
 
-1.이분법 이용
+이분법 이용
     a1,b1사이에 근 존재-> cn=(an+bn)/2
-    bn-cn <= 오차 -> break, cn이 수치해
+    bn-an <= 오차 -> break, cn이 수치해
     안끝나면: f(an)f(cn)<=0 ->an+1 = an, bn+1 = cn
                        >=0 ->an+1 = cn, bn+1 = b 
 소수의 정확한 연산
@@ -28,7 +26,7 @@ f'(x)=A+Bcos(x)
 import sys
 import decimal
 
-# Decimal의 정밀도를 50자리까지 설정
+# Decimal의 정밀도를 100자리까지 설정
 decimal.getcontext().prec = 100
 decimal.getcontext().rounding = decimal.ROUND_HALF_UP
 
