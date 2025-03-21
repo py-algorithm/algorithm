af1fd21317d00dfa4bfebe537c0d501764eeacfe
99mini <itman390@gmail.com>
2025-03-21 09:09:17 +0900

    feat: 그리드 알고리즘으로 해결

--- Programmers/99mini/연습문제/기지국설치.js
+++ Programmers/99mini/연습문제/기지국설치.js
@@ -1,30 +1,29 @@
 /**
  * @link https://school.programmers.co.kr/learn/courses/30/lessons/12979
  * 카테고리
- *   - BFS
- *   - DFS
+ *   - 그리디
  * 문제풀이
- * 알파벳 후 순 정렬. DFS 탐색
+ * 시작점과 끝점을 갱신하며 그리드 알고리즘 구현
  */
 function solution(n, stations, w) {
   var answer = 0;
+  const range = 2 * w + 1;
+  let start = 1;
+  let end = 0;
 
-  const arr = Array.from({ length: n }).fill(0);
+  for (const s of stations) {
+    end = s - w - start;
 
-  stations.forEach((s) => {
-    for (let i = Math.max(s - w - 1, 0); i < Math.min(s + w, n); i++) {
-      arr[i] = 1;
+    if (s - w > start) {
+      answer += Math.ceil(end / range);
     }
-  });
 
-  for (let i = 0; i < n - w; i += w) {
-    if (!arr[i]) {
-      console.log(i);
-      answer++;
-      for (let j = Math.max(i - w, 0); j < Math.min(i + w + 1, n); j++) {
-        arr[j] = 1;
-      }
-    }
+    start = s + w + 1;
+  }
+
+  if (start <= n) {
+    end = n + 1 - start;
+    answer += Math.ceil(end / range);
   }
 
   return answer;
