23163f7d19178c5964f3c7263d2e8e9f0e4fc8d6
99mini <itman390@gmail.com>
2025-05-10 15:52:51 +0900

    feat: 벽부수고 이동하기 2 큐 직접 구현 (연결 리스트 형태)

---
+++ Baekjoon/99mini/14442.js
@@ -0,0 +1,106 @@
+/**
+ * @author 99mini
+ * @see https://www.acmicpc.net/problem/14442
+ * @tag BFS, 그래프 탐색
+ * @description
+ */
+function solution(n, m, k, grid) {
+  class Queue {
+    constructor() {
+      this.head = null;
+      this.tail = null;
+      this.length = 0;
+    }
+    push(val) {
+      const newItem = { data: val, next: null };
+      if (this.head === null) {
+        this.head = newItem;
+      } else {
+        this.tail.next = newItem;
+      }
+      this.tail = newItem;
+      this.length++;
+    }
+    popLeft() {
+      const val = this.head.data;
+      this.head = this.head.next;
+      this.length--;
+      return val;
+    }
+  }
+  const q = new Queue();
+  q.push([0, 0, 0]);
+
+  /**
+   * @description n by m by k 3d array
+   * @type number[][][]
+   */
+  const visited = Array.from({ length: n }, () =>
+    Array.from({ length: m }, () => Array.from({ length: k }).fill(0))
+  );
+
+  visited[0][0][0] = true;
+
+  const dn = [
+    [0, 1],
+    [0, -1],
+    [1, 0],
+    [-1, 0],
+  ];
+
+  while (q.length) {
+    const [row, col, cnt] = q.popLeft();
+
+    if (row === n - 1 && col === m - 1) {
+      return visited[row][col][cnt];
+    }
+
+    for (const [dr, dc] of dn) {
+      const nr = dr + row;
+      const nc = dc + col;
+
+      // out of boundary
+      if (0 > nr || nr >= n || 0 > nc || nc >= m) {
+        continue;
+      }
+
+      const cell = grid[nr][nc];
+
+      // if wall and broken count is less than k
+      if (cell === "1" && cnt < k && !visited[nr][nc][cnt + 1]) {
+        q.push([nr, nc, cnt + 1]);
+        visited[nr][nc][cnt + 1] = visited[row][col][cnt] + 1;
+      }
+      // if is not wall
+      else if (cell === "0" && !visited[nr][nc][cnt]) {
+        q.push([nr, nc, cnt]);
+        visited[nr][nc][cnt] = visited[row][col][cnt] + 1;
+      }
+    }
+  }
+
+  return -1;
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: "OHAI> ",
+});
+
+const lines = [];
+
+rl.on("line", (line) => {
+  /** 입력 받는 영역 */
+  lines.push(line.split(" "));
+}).on("close", () => {
+  /** 문제 해결 영역 */
+  const [N, M, K] = lines[0].map(Number);
+
+  const grid = lines.slice(1).map((v) => String(v).split(""));
+
+  console.log(solution(N, M, K, grid));
+
+  process.exit(0);
+});
