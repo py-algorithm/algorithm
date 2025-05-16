fcfb2291378834e9225b5620a0530eb037d5a973
99mini <itman390@gmail.com>
2025-05-16 12:47:07 +0900

    feat: implement queue. pop, push, front, rear, length

--- javascript/collections/queue/queue.test.ts
+++ javascript/collections/queue/queue.test.ts
@@ -3,6 +3,26 @@ import { Queue } from "./queue";
 describe("Queue", () => {
   test("should be defined", () => {
     const queue = new Queue();
+
     expect(queue).toBeDefined();
+  });
+
+  test("should be init", () => {
+    const queue = new Queue([1, 2, 3]);
+
+    expect(queue.length).toBe(3);
+  });
+
+  test("should be pop front item", () => {
+    const queue = new Queue([1, 2, 3]);
+
+    expect(queue.pop()).toBe(1);
+  });
+
+  test("should be push rear item", () => {
+    const queue = new Queue([1, 2, 3]);
+    queue.push(4);
+    expect(queue.front).toBe(1);
+    expect(queue.rear).toBe(4);
   });
 });
