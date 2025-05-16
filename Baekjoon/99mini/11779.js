96c589cd356f751c94dc46a07366f49ae7da31c4
99mini <itman390@gmail.com>
2025-05-17 08:34:54 +0900

    11779 임시 저장

--- Baekjoon/99mini/11779.js
+++ Baekjoon/99mini/11779.js
@@ -13,7 +13,88 @@
  * @param {number} start
  * @param {number} end
  */
-function solution(n, m, graph, start, end) {}
+function solution(n, m, graph, start, end) {
+  const queue = () => {
+    let head = null;
+    let tail = null;
+    let length = 0;
+
+    return {
+      get length() {
+        return length;
+      },
+      pop: () => {
+        const ret = head.value;
+        head = head.next;
+        length--;
+        return ret;
+      },
+      push: (value) => {
+        const newItem = { value, next: null };
+        if (head === null) {
+          head = newItem;
+        } else {
+          tail.next = newItem;
+        }
+        tail = newItem;
+        length++;
+      },
+    };
+  };
+
+  /**
+   * @type Record<number, Record<number, number>>
+   */
+  const g = {};
+
+  graph.forEach(([s, e, w]) => {
+    if (s in g) {
+      if (e in g[s]) {
+        g[s][e] = Math.min(g[s][e], w);
+      } else {
+        g[s][e] = w;
+      }
+    } else {
+      g[s] = {};
+      g[s][e] = w;
+    }
+  });
+
+  const targetEdge = g[start];
+
+  /**
+   * @type number[]
+   */
+  const dist = Array.from({ length: n }).fill(Number.MAX_SAFE_INTEGER);
+
+  dist[start - 1] = 0;
+
+  function bfs(s) {
+    const q = queue();
+    q.push(s);
+
+    while (q.length) {
+      const currentNode = q.pop();
+
+      const nextNodes = Object.entries(g[currentNode]);
+
+      for (const [node, weight] of nextNodes) {
+        const nextWeight = dist[node - 1] + g[node];
+
+        if (nextWeight > dist[node - 1]) {
+          continue;
+        }
+
+        q.push(node);
+        dist[node - 1] = nextWeight;
+      }
+    }
+  }
+
+  bfs(start);
+
+  return dist[end - 1];
+}
 
 const readline = require("readline");
 const rl = readline.createInterface({
