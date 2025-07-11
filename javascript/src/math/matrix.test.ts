c19fc9b10679058ef37f752bd864058846e2508e
99mini <itman390@gmail.com>
2025-07-11 12:44:01 +0900

    feat: reverse col

--- javascript/src/math/matrix.test.ts
+++ javascript/src/math/matrix.test.ts
@@ -1,20 +1,40 @@
-import { reverseRow } from "./matrix";
+import { reverseCol, reverseRow } from "./matrix";
+
+const matrix = [
+  [1, 2, 3, 4],
+  [5, 6, 7, 8],
+  [9, 10, 11, 12],
+];
+
+describe("reverseCol", () => {
+  it("should reverseCol", () => {
+    const reversed = reverseCol(1, matrix);
+    console.log(reversed);
+    expect(reversed).toEqual([
+      [1, 10, 3, 4],
+      [5, 6, 7, 8],
+      [9, 2, 11, 12],
+    ]);
+  });
+
+  it("throw error if out of index", () => {
+    try {
+      const outOfIndexErrorRes = reverseCol(4, matrix);
+    } catch (error) {
+      expect(error.message).toBe(
+        `Matrix Index Out of Range\nnMatrix col: ${4}, c: ${4}`
+      );
+    }
+  });
+});
 
 describe("reverseRow", () => {
-  const matrix = [
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12],
-    [13, 14, 15, 16],
-  ];
-
   it("should reverseRow", () => {
     const reversed = reverseRow(1, matrix);
     expect(reversed).toEqual([
       [1, 2, 3, 4],
       [8, 7, 6, 5],
       [9, 10, 11, 12],
-      [13, 14, 15, 16],
     ]);
   });
 
@@ -22,7 +42,9 @@ describe("reverseRow", () => {
     try {
       const outOfIndexErrorRes = reverseRow(4, matrix);
     } catch (error) {
-      expect(error.message).toBe(`Matrix Index Out of Range\nr: ${4}`);
+      expect(error.message).toBe(
+        `Matrix Index Out of Range\nMatrix row: ${3}, r: ${4}`
+      );
     }
   });
 });
--- javascript/src/math/matrix.ts
+++ javascript/src/math/matrix.ts
@@ -1,13 +1,36 @@
 /**
+ *
+ * @param c - target col 0-indexed
+ * @param matrix
+ */
+export function reverseCol(c: number, matrix: number[][]): number[][] {
+  const col = matrix[0].length;
+
+  if (c < 0 || c >= col) {
+    throw new Error(`Matrix Index Out of Range\nnMatrix col: ${col}, c: ${c}`);
+  }
+
+  const newMatrix = [...matrix.map((row) => [...row])];
+
+  for (let i = 0; i < matrix.length; i++) {
+    const temp = matrix[i][c];
+    newMatrix[i][c] = matrix[matrix.length - 1 - i][c];
+    newMatrix[matrix.length - 1 - i][c] = temp;
+  }
+
+  return newMatrix;
+}
+
+/**
  * @description reverse row
- * @param r
+ * @param r - target row 0-indexed
  * @param matrix
  */
 export function reverseRow(r: number, matrix: number[][]): number[][] {
   const row = matrix.length;
 
   if (r < 0 || r >= row) {
-    throw new Error(`Matrix Index Out of Range\nr: ${r}`);
+    throw new Error(`Matrix Index Out of Range\nMatrix row: ${row}, r: ${r}`);
   }
   const reversedRow = matrix[r].reverse();
 
